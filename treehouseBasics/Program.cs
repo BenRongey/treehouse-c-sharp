47b5c3e41b4828eb2165300e27c3018e71011539
Benjamin Rongey <benjamin.rongey@gmail.com>
2019-09-30 20:11:01 -0500

    minor changes and additions to basic hello world

--- treehouseBasics/Program.cs
+++ treehouseBasics/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Threading;
 
 namespace treehouseBasics
+
 {
     class Program
     {
+
+        static void Ask(string question) {
+            Console.WriteLine(question);
+            string answer = Console.ReadLine();
+            Console.WriteLine(answer);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Ask("How many cans are you ordering?");
         }
     }
 }
